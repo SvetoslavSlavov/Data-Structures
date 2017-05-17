5ea84ee043ea90f0bfbd5dee80c327e77213a783
Svetoslav Slavov <svetoslav_slavov@icloud.com>
2017-05-17 20:15:13 +0300

    Queue and TreeNode exercise

---
+++ Queue/Queue/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("Message One");
+            queue.Enqueue("Message Two");
+            queue.Enqueue("Message Three");
+            queue.Enqueue("Message Four");
+
+            while (queue.Count>0)
+            {
+                string msg = queue.Dequeue();
+                Console.WriteLine(msg);
+            }
+        }
+    }
+}
